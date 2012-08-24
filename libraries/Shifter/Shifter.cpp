e080258aab9f38621037a12c32e5e8111703ea31
David Rochwerger <catch.dave@gmail.com>
2012-08-24 00:07:03 -0700

    Added internal state to shifter, so we only need to send data once a cycle.

--- libraries/Shifter/Shifter.cpp
+++ libraries/Shifter/Shifter.cpp
@@ -4,72 +4,79 @@
 
 
 // Constructor
-Shifter::Shifter(int SER_Pin, int RCLK_Pin, int SRCLK_Pin, int Number_of_Registers){  
-    _SER_Pin = SER_Pin;
-    _RCLK_Pin = RCLK_Pin;
-    _SRCLK_Pin = SRCLK_Pin;
-    
-    _Number_of_Registers = Number_of_Registers;
-    
-    
-    pinMode(_SER_Pin, OUTPUT);
-    pinMode(_RCLK_Pin, OUTPUT);
-    pinMode(_SRCLK_Pin, OUTPUT);
-    
-    
-    clear(); //reset all register pins
-    write();
+Shifter::Shifter(int SER_Pin, int RCLK_Pin, int SRCLK_Pin, int Number_of_Registers){
+	_SER_Pin = SER_Pin;
+	_RCLK_Pin = RCLK_Pin;
+	_SRCLK_Pin = SRCLK_Pin;
+
+	_Number_of_Registers = Number_of_Registers;
+
+
+	pinMode(_SER_Pin, OUTPUT);
+	pinMode(_RCLK_Pin, OUTPUT);
+	pinMode(_SRCLK_Pin, OUTPUT);
+
+
+	clear(); //reset all register pins
+	write();
 }
 
 void Shifter::write(){
-    //Set and display registers
-    //Only call AFTER all values are set how you would like (slow otherwise)
+	//Set and display registers
+	//Only call AFTER all values are set how you would like (slow otherwise)
 
- digitalWrite(_RCLK_Pin, LOW);
- 
- //iterate through the registers
- for(int i = _Number_of_Registers - 1; i >= 0; i--){
-  
-  //iterate through the bits in each registers
-  for(int j = 8 - 1; j >= 0; j--){
-   
-   digitalWrite(_SRCLK_Pin, LOW);  
-   
-   int val = _shiftRegisters[i] & (1 << j);
-   
-   digitalWrite(_SER_Pin, val);
-   digitalWrite(_SRCLK_Pin, HIGH);
-  
+  digitalWrite(_RCLK_Pin, LOW);
+
+  //iterate through the registers
+  for(int i = _Number_of_Registers - 1; i >=  0; i--){
+
+    //iterate through the bits in each registers
+    for(int j = 8 - 1; j >=  0; j--){
+
+      digitalWrite(_SRCLK_Pin, LOW);
+
+      int val = _shiftRegisters[i] & (1 << j);
+
+      digitalWrite(_SER_Pin, val);
+      digitalWrite(_SRCLK_Pin, HIGH);
+
+    }
+
   }
- 
- }
- 
- digitalWrite(_RCLK_Pin, HIGH);
+
+  digitalWrite(_RCLK_Pin, HIGH);
+  _updateNeeded = false;
 }
-    
+
 void Shifter::setPin(int index, boolean val){
-    int byteIndex = index/8;
-    int bitIndex = index % 8;
-    
-    byte current = _shiftRegisters[byteIndex];
-    
-    current &= ~(1 << bitIndex); //clear the bit
-    current |= val << bitIndex; //set the bit
-    
-    _shiftRegisters[byteIndex] = current; //set the value
+	int byteIndex = index/8;
+	int bitIndex = index % 8;
+
+	byte current = _shiftRegisters[byteIndex];
+
+	current &= ~(1 << bitIndex); //clear the bit
+	current |= val << bitIndex; //set the bit
+
+	_shiftRegisters[byteIndex] = current; //set the value
+  _updateNeeded = true;
 }
 
 void Shifter::setAll(boolean val){
-//set all register pins to LOW 
- for(int i = _Number_of_Registers * 8 - 1; i >= 0; i--){
-  setPin(i, val);
- }
+//set all register pins to LOW
+  for(int i = _Number_of_Registers * 8 - 1; i >=  0; i--){
+     setPin(i, val);
+  }
 }
 
 
 void Shifter::clear(){
-//set all register pins to LOW 
- for(int i = _Number_of_Registers * 8 - 1; i >= 0; i--){
-  setPin(i, LOW);
- }
+//set all register pins to LOW
+  for(int i = _Number_of_Registers * 8 - 1; i >=  0; i--){
+     setPin(i, LOW);
+  }
+}
+
+boolean Shifter::isUpdateNeeded()
+{
+  return _updateNeeded;
 }
--- libraries/Shifter/Shifter.h
+++ libraries/Shifter/Shifter.h
@@ -7,22 +7,24 @@
 // Define the Shifter class
 class Shifter
 {
- public:
-  // Constructor
-  Shifter(int SER_Pin, int RCLK_Pin, int SRCLK_Pin, int Number_of_Registers);
-    void write();
-    void setPin(int index, boolean val);
-    void setAll(boolean val);
-    void clear();
+  public:
+    // Constructor
+    Shifter(int SER_Pin, int RCLK_Pin, int SRCLK_Pin, int Number_of_Registers);
+   	void write();
+   	void setPin(int index, boolean val);
+   	void setAll(boolean val);
+   	void clear();
+    boolean isUpdateNeeded();
 
 
 
- private:
-    int _SER_Pin;
-    int _RCLK_Pin;
-    int _SRCLK_Pin;
-    int _Number_of_Registers;
-    byte _shiftRegisters[25];
+  private:
+	int _SER_Pin;
+  	int _RCLK_Pin;
+  	int _SRCLK_Pin;
+  	int _Number_of_Registers;
+  	byte _shiftRegisters[25];
+    boolean _updateNeeded;
 };
 
 #endif //Shifter_h
