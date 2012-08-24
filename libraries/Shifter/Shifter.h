cf964e82882385d97c76de44ec0dae58f87817d0
David Rochwerger <catch.dave@gmail.com>
2012-08-24 00:05:30 -0700

    Add Shifter library (see: http://bildr.org/2011/08/74hc595-breakout-arduino/)

---
+++ libraries/Shifter/Shifter.h
@@ -0,0 +1,28 @@
+#ifndef Shifter_h
+#define Shifter_h
+
+// Include the standard types
+#include <Arduino.h>
+
+// Define the Shifter class
+class Shifter
+{
+ public:
+  // Constructor
+  Shifter(int SER_Pin, int RCLK_Pin, int SRCLK_Pin, int Number_of_Registers);
+    void write();
+    void setPin(int index, boolean val);
+    void setAll(boolean val);
+    void clear();
+
+
+
+ private:
+    int _SER_Pin;
+    int _RCLK_Pin;
+    int _SRCLK_Pin;
+    int _Number_of_Registers;
+    byte _shiftRegisters[25];
+};
+
+#endif //Shifter_h
